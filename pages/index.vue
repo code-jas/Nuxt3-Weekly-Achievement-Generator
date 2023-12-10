fd25087a458ff6850130f98694d7711b00caa65a
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2023-12-10 19:26:42 +0800

    change ui

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import moment from 'moment';
 
+
 //DATA
 const state = reactive({ 
     // data
@@ -18,10 +19,10 @@ const state = reactive({
 })
 
 const columns = [
-    {title: "Date", dataIndex: "date", key: "date", scopedSlots: { customRender: "date" }, width: 150},
-    {title: "Descriptions", dataIndex: "description", key: "description", width: 700},
-    {title: "Start Time", dataIndex: "startTime", key: "startTime", width: 150, ellipsis: true,},
-    {title: "End Time", dataIndex: "endTime", key: "endTime", width: 150, ellipsis: true,},
+    {title: "Date", dataIndex: "date", key: "date", width: 110},
+    {title: "Descriptions", dataIndex: "description", key: "description"},
+    {title: "Start Time", dataIndex: "startTime", key: "startTime", width: 130, ellipsis: true,},
+    {title: "End Time", dataIndex: "endTime", key: "endTime", width: 130, ellipsis: true,},
     {title: "Duration", dataIndex: "duration", key: "duration", width: 150, ellipsis: true, },
 ]
     
@@ -93,7 +94,8 @@ fetchData()
 
 
 <template>
-    <div style="margin: 40px">
+<div class="bg-home">
+    <div style="margin: 40px" >
         <a-row type="flex" justify="start" align="top" :gutter="[30, 20]">
             <a-col :span="12" justify="center" align="left">
                 <a-range-picker @change="dateRangeOnChange" :size="state.size" style="width:400px;" />
@@ -120,7 +122,15 @@ fetchData()
                 <!-- <pre>{{ state.accomplishmentReports }}</pre> -->
                 <a-spin tip="Loading..." class="center-loading" :spinning="state.fetchLoading">
                     <div v-if="state.user">
-                        <a-table :columns="columns" :data-source="state.accomplishmentReports" bordered size="small" :pagination="{ pageSize: 30 }"  >
+                        <a-table 
+                            :columns="columns" 
+                            :data-source="state.accomplishmentReports" 
+                            bordered 
+                            size="small" 
+                            :pagination="{ pageSize: 30 }" 
+                            :scroll="{ x: 1000}"
+                            
+                        >
                             <template #bodyCell="{column, text, record}">
                                 <template v-if="column.dataIndex === 'duration'">
                                     {{ record.formattedTotalDurationPerDay  ? record.formattedTotalDurationPerDay : text }}
@@ -129,7 +139,7 @@ fetchData()
                         </a-table>
                     </div>
                     <div v-else class="center-loading">
-                        <h1>INPUT YOUR DETAILS HAHAHA</h1>
+                        <h1 style="color: #fff; text-align: center; margin: 5rem;">PLEASE INPUT YOUR DETAILS FIRST</h1>
                     </div>
                 </a-spin>
             </a-col>
@@ -138,5 +148,19 @@ fetchData()
         </a-row>
         <InputModal :open="state.showModal" @close="state.showModal = false"  @reload="fetchData"/>
     </div>
+</div>
 </template>
 
+
+<style>
+.bg-home{ 
+  display:flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100vw;
+  background-image: url('~/assets/bg.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+</style>
