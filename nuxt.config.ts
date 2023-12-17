a3d26f5999b3c03036cc5314fccae9e0613ca86d
Angelo Silvestre <johnangelo.silvestre04@gmail.com>
2023-12-17 18:29:32 +0800

    fix conflict

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,17 +37,9 @@ export default defineNuxtConfig({
     workspaceId: process.env.CLOCKIFY_WORKSPACE_ID,
     userId: process.env.CLOCKIFY_USER_ID,
     isDeployed: process.env.IS_DEPLOYED,
-   
-
     // Public keys that are exposed to the client
     public: { 
     }
   },
-<<<<<<< Updated upstream
  
-=======
-  // nitro: {
-  //   preset: 'node-server'`
-  // }`
->>>>>>> Stashed changes
 })
